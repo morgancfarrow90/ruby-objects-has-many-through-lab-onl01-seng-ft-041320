146cee86ba0c5bca430e6af0cee3a6fafb55b8f1
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-02 11:13:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor
 
-attr_accessor :name  
+attr_accessor :name, :appointment, :patient 
   
 @@all = []
 
@@ -8,5 +8,7 @@ def initialize(name)
   @name = name
   @@all << self
 end
+
+
   
 end
